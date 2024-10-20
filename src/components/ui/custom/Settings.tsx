30f9585181b2ed7cfbff2675bcfeabafe855f138
mohsinziia <ziamohsin409@gmail.com>
2024-10-20 16:50:22 +0500

    Update settings ui

--- src/components/ui/custom/Settings.tsx
+++ src/components/ui/custom/Settings.tsx
@@ -33,7 +33,7 @@ const Settings = ({
               <Link
                 className={`${
                   window === "Profile" && "bg-muted hover:bg-muted"
-                } inline-flex rounded-md items-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:text-accent-foreground h-9 px-4 py-2 hover:bg-transparent hover:underline justify-start`}
+                } inline-flex rounded-md items-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:text-accent-foreground h-9 px-4 py-2 justify-start`}
                 href="#"
                 onClick={() => {
                   return handleWindowChange("Profile");
@@ -45,7 +45,7 @@ const Settings = ({
               <Link
                 className={`${
                   window === "Appearance" && "bg-muted hover:bg-muted"
-                } inline-flex rounded-md items-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:text-accent-foreground h-9 px-4 py-2 hover:bg-transparent hover:underline justify-start`}
+                } inline-flex rounded-md items-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:text-accent-foreground h-9 px-4 py-2 justify-start`}
                 href="#"
                 onClick={() => {
                   return handleWindowChange("Appearance");
